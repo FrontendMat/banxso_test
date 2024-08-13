dbdd17662849235518dcd232072f3a6cae850678
SomeBodyM <151923587+SomeBodyM@users.noreply.github.com>
2024-08-13 21:27:15 +0300

    url fix

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         ],
     },
     define: {
-        __API__: JSON.stringify('https://banxo-tets-server.vercel.app/api/'),
+        __API__: JSON.stringify('https://banxo-tets-server.vercel.app/api'),
     },
     build: {
         rollupOptions: {
